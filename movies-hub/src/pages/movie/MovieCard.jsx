cc4bf1340db495507b5dcaff705fbfc689a2f5bf
Kamran Sajjad <kamransajjad2580@gmail.com>
2025-09-05 18:06:07 +0500

    fix error

--- movies-hub/src/pages/movie/MovieCard.jsx
+++ movies-hub/src/pages/movie/MovieCard.jsx
@@ -1,40 +1,4 @@
-// // src/components/Movies/MovieCard.jsx
-// import React from "react";
-// import { Navigate, useNavigate } from "react-router-dom";
 
-// const MovieCard = ({ movie, onClick }) => {
-//   const navigate = useNavigate();
-//   const handleClick = () => onClick(movie);
-//   return (
-//     <div
-//       className="bg-gray-800 rounded-xl shadow-md overflow-hidden hover:scale-105 transform transition duration-300 cursor-pointer"
-//       onClick={handleClick}
-//     >
-//       <img
-//         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//         alt={movie?.title || "Movie"}
-//         className="w-full h-80 object-cover"
-//       />
-//       <div className="p-4">
-//         <h3 className="text-lg font-semibold text-white truncate">
-//           {movie?.title}
-//         </h3>
-//         <p className="text-sm text-gray-400 mt-1">
-//           ⭐ {movie?.vote_average?.toFixed(1) || "N/A"}
-//         </p>
-//         <p className="text-sm text-gray-400 mt-1 line-clamp-2">
-//           {movie?.overview || "No description available."}
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MovieCard;
-
-
-
-// src/components/Movies/MovieCard.jsx
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Heart } from "lucide-react";
@@ -44,15 +8,14 @@ const MovieCard = ({ movie, onClick }) => {
 
   const [isFavorite, setIsFavorite] = useState(false);
 
-  // Load favorites from localStorage
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
     setIsFavorite(storedFavorites.some((fav) => fav.id === movie.id));
   }, [movie.id]);
 
-  // Toggle favorite
+ 
   const handleFavorite = (e) => {
-    e.stopPropagation(); // prevent navigating when clicking heart
+    e.stopPropagation(); 
     const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
     let updatedFavorites;
