9b060ed5a67b6cc88ca6f1b016b9c2fc3340a232
Kamran Sajjad <kamransajjad2580@gmail.com>
2025-09-15 16:13:32 +0500

    remove comments

--- movies-hub/src/pages/auth/SignupPage.jsx
+++ movies-hub/src/pages/auth/SignupPage.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { useMutation } from "@tanstack/react-query";
-
-import { successToast, errorToast } from "../../utils/displayToast";
-import { AUTH_API } from "../../lib/api";
+import { successToast } from "../../utils/displayToast";
 import { ROUTES } from "../../routes/routeConstants";
 import { Button } from "../../components/ui/Button";
 import { AuthFormWrapper } from "../../components/layout/AuthFormWrapper";
-import { useAuthStore } from "../../lib/store/useAuthStore";
-import { API_TOKEN } from "../../lib/constant/constants";
 import { signupFieldsData } from "../../utils/data/FieldData";
 import { InputFieldSelection } from "../../lib/factory/InputFieldSelection";
 import { zodResolver } from "@hookform/resolvers/zod";
 const SignupPage = () => {
   const navigate = useNavigate();
-  const { setToken } = useAuthStore();
 
   const submitFormData = (payload) => {
     console.log(payload);
